f7420a820c89b7426d29b952ec216f88ff3d4d81
MkArs <arsen_mkhitaryan_2000@mail.ru>
2022-08-06 11:48:08 +0300

    Initial commit

---
+++ src/main/java/ru/mkhitaryan/spring/models/Person.java
@@ -0,0 +1,28 @@
+package ru.mkhitaryan.spring.models;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(){};
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
